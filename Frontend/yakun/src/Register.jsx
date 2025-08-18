f33bebca4e3a5db562c2ae9531cb0d8cbf5c20af
mrauthentik <umokeuchenna2020@gmail.com>
2025-08-18 10:59:51 +0100

    made some changes to the register page

--- Frontend/yakun/src/Register.jsx
+++ Frontend/yakun/src/Register.jsx
@@ -1,11 +1,40 @@
-
- const Register = ()=>{
+import { useNavigate, Link} from 'react-router-dom'
+import { useState } from 'react'
+const Register = ()=> {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [name, setName] = useState('')
+    const navigate = useNavigate()
     return (
-        <div className="register">
-            <h1>Register all your Information</h1>
-            
+        <div className="login">
+            <h1>Register all information</h1>
+            <form >
+                <input 
+                    type="text" 
+                    placeholder="input name" 
+                    required 
+                    value={name}
+                    onChange={(e)=>setName(e.target.value)}
+                    />
+                <input 
+                    type="email" 
+                    placeholder="input email" 
+                    required 
+                    value={email}
+                    onChange={(e)=>setEmail(e.target.value)}
+                    />
+                <input 
+                    type="password" 
+                    placeholder="input password" 
+                    required 
+                    value={password}
+                    onChange={(e)=>setPassword(e.target.value)}
+                    />
+                <button> Submit</button>
+            </form>
         </div>
     )
 }
+
 
 export default Register
